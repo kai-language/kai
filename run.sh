c9e029ca5b9b6fab59818c4e9de914233751dd55
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-09-10 22:10:25 +1000

    Return the build tooling
    Signed-off-by: Ethan Jackwitz <ethanjackwitz@gmail.com>

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./build.sh
+
+if [ $? -ne 0 ]; then
+
+  echo "Build Failed!"
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+
+  .build/debug/kai samples/main.kai
+else
+
+  .build/debug/kai $1
+fi
