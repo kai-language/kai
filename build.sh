f60bfd879648c94eaa1d058888441836279876a4
Brett Toomey <brettcwx@gmail.com>
2016-09-28 15:34:34 +0900

    Removed swiftenv from build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Must have swiftenv installed here. Default for homebrew
-CC=`/usr/local/bin/swiftenv which swift`
+CC="swift"
 
 #echo "Using $CC"
 export SDKROOT=$(xcrun --show-sdk-path --sdk macosx)
