d5037b204039f0c3548a668a127e9555a9775e4b
Brett Toomey <brettcwx@gmail.com>
2016-09-29 01:36:41 +0900

    Very primitive type solving.

--- Sources/kai/main.swift
+++ Sources/kai/main.swift
@@ -44,13 +44,15 @@ let file = File(path: filePath)!
 do {
 
     var lexer = Lexer(file)
-
-    let ast = try Parser.parse(&lexer)
-
+    console.warning("-----------------Parser-----------------")
+    var ast = try Parser.parse(&lexer)
     print(ast.pretty())
-
-} catch {
-  print(error)
+    console.warning("---------------Type Solver--------------")
+    try TypeSolver.run(on: &ast)
+    print(ast.pretty())
+    
+} catch let error as CompilerError {
+  console.error(error.description)
 }
 
 // print(parserGrammer.pretty())
