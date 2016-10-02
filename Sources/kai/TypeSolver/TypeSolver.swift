ef0099fe3295cb8bd33ba743f9b26311ace6378f
Brett Toomey <brettcwx@gmail.com>
2016-10-02 09:40:35 -0700

    Making type solver a little more open.

--- Sources/kai/TypeSolver/TypeSolver.swift
+++ Sources/kai/TypeSolver/TypeSolver.swift
@@ -19,9 +19,7 @@ class TypeSolver {
                 break
             case .declaration(let symbol) where symbol.types.first == nil:
                 guard child.children.count > 0 else { print("what no children!!!"); continue }
-                
-                //drop the identifier token
-                guard let type = try solve(given: child.children) else {
+                guard let type = try solve(given: child.children, in: symbol) else {
                     print("unable to solve type")
                     return
                 }
@@ -35,36 +33,70 @@ class TypeSolver {
         }
     }
     
-    func solve(given nodes: [AST]) throws -> KaiType? {
+    func solve(given nodes: [AST], in scope: Symbol) throws -> KaiType? {
         var makingProgress = true
         while makingProgress {
             makingProgress = false
             
-            for node in nodes {
+            for (index, node) in nodes.enumerated() {
                 switch node.kind {
-                case .integer(let value): return KaiType.integer(value)
-                case .string(let value): return KaiType.string(value)
+                case .integer(_): return KaiType.integer
+                case .real(_): return KaiType.float
+                case .string(_): return KaiType.string
+                case .boolean(_): return KaiType.boolean
                 
+                case .operator(let `operator`):
+                    return try solve(given: node.children, in: scope)
+
                 //this is either a user defined type or an error
-                case .identifier(let name):
-                    //TODO(Brett): look for `identifier` in the symbol table
-                    //if the symbol isn't in the table throw an "unidentified
-                    //symbol error."
-                    
-                    //findSymbol()
-                    break
+                case .identifier(let name) where index != 0:
+                    guard let type = try findSymbol(named: name, in: scope)?.types.first else {
+                        print("unidentified symbol: \(name) \(node.parent)")
+                        return nil
+                    }
+                    return type
+
                 default:
                     continue
                 }
             }
-            
         }
-        
         return nil
     }
     
-    func findSymbol() throws {
-        //TODO(Brett): build me
+    func findSymbol(named name: ByteString, in scope: Symbol) throws -> Symbol? {
+        /*guard let symbol = scope.lookup(name) else {
+            return nil
+        }
+
+        guard let type = symbol.types.firt else {
+            //FIXME(Brett) needs to do a recursive solve for
+            //undefined symbol
+            return nil
+        }
+
+        //make sure type isn't a user-defined type, otherwise
+        //look up the definition of the type and return it
+        switch type {
+        case .identifier:
+            return try findSymbol(named: name, in: scope)
+        default:
+            return symbol
+       }*/
+       
+       //FIXME(Brett): need to modify `Symbol` and `SymbolTable` to be able to hold
+       //an index for their location in `SymbolTable.symbols`
+        guard 
+            let symbol = SymbolTable.global.lookup(name),
+            let type = symbol.types.first
+        else { return nil }
+
+        switch type {
+        //case .other(let parentSymbol):
+          //  return try parentSymbol
+        default:
+            return symbol
+        }
     }
 }
 
@@ -79,10 +111,3 @@ extension TypeSolver {
         }
     }
 }
-
-
-
-
-
-
-
