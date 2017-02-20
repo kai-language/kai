8f4599c4c3709c4042d07b6eabc28e31031f3978
Brett Toomey <brettcwx@gmail.com>
2017-02-20 12:51:57 -0800

    Printf fixes

--- Sources/kai/IRGenerator/ValueTypes.swift
+++ Sources/kai/IRGenerator/ValueTypes.swift
@@ -34,8 +34,16 @@ extension IRGenerator {
     
     func emitGlobalString(name: String? = nil, value: ByteString) -> IRValue {
         return builder.buildGlobalStringPtr(
-            value.string,
+            value.string.escaped,
             name: name ?? ""
         )
+    }
+}
+
+extension String {
+    //TODO: More robust system
+    var escaped: String {
+        return self.replacingOccurrences(of: "\\n", with: "\n")
+            .replacingOccurrences(of: "\\t", with: "\t")
     }
 }
