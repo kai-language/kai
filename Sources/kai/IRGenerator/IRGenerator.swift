ec9ca3862b25f9c69298789f3b788defd356d229
Ethan Jackwitz <ethanjackwitz@gmail.com>
2017-03-29 11:04:18 +1000

    Added if statements & fixed bug in for statements

--- Sources/kai/IRGenerator/IRGenerator.swift
+++ Sources/kai/IRGenerator/IRGenerator.swift
@@ -133,6 +133,13 @@ extension IRGenerator {
         case .litString, .litFloat, .litInteger, .litProc:
             return emitLiteral(for: node)
 
+        // FIXME(vdka): Feels a bit hacky.
+        case .ident("true", _):
+            return IntType.int1.constant(1)
+
+        case .ident("false", _):
+            return IntType.int1.constant(0)
+
         case .ident(let identifier, _):
             let entity = context.scope.lookup(identifier)!
             return builder.buildLoad(llvmPointers[entity]!)
@@ -178,6 +185,9 @@ extension IRGenerator {
         case .stmtReturn:
             return emitReturnStmt(for: node)
 
+        case .stmtIf:
+            return emitIfStmt(for: node)
+
         case .stmtFor:
             return emitForStmt(for: node)
 
@@ -192,134 +202,64 @@ extension IRGenerator {
         }
     }
 
-    #if false
-    // NOTE(vdka): Because this emits both exprs and stmts it should be named emitStmt
-    func emitExpression(for node: AstNode) throws -> IRValue {
+    func emitIfStmt(for node: AstNode) -> IRValue {
+        guard case .stmtIf(let cond, let thenStmt, let elseStmt, _) = node else {
+            panic()
+        }
 
-        switch node.kind {
-        //NOTE(Brett): how do we want to handle different values here, should we
-        // continue to ignore them and just return nil?
-        case .scope(_):
-            for child in node.children {
-                _ = try emitExpression(for: child)
+        let curFunction = builder.currentFunction!
+        let thenBlock: BasicBlock = curFunction.appendBasicBlock(named: "if.then")
+        var elseBlock: BasicBlock?
+        let postBlock: BasicBlock
+
+        if elseStmt != nil {
+            elseBlock = curFunction.appendBasicBlock(named: "if.else")
+        }
+
+        postBlock = curFunction.appendBasicBlock(named: "if.post")
+
+        //
+        // Emit the conditional branch
+        //
+
+        let condVal = emitConditional(for: cond)
+
+        builder.buildCondBr(condition: condVal, then: thenBlock, else: elseBlock ?? postBlock)
+
+        //
+        // Emit the `then` block
+        //
+
+        builder.positionAtEnd(of: thenBlock)
+
+        emitStmt(for: thenStmt)
+
+        if !thenBlock.hasTerminatingInstruction {
+            builder.buildBr(postBlock)
+        }
+
+        if let elseBlock = elseBlock, let elseStmt = elseStmt {
+
+            //
+            // Emit the `else` block
+            //
+
+            builder.positionAtEnd(of: elseBlock)
+
+            emitStmt(for: elseStmt)
+
+            if !elseBlock.hasTerminatingInstruction {
+                builder.buildBr(postBlock)
             }
-            return VoidType().null()
-            
-        case .assignment:
-            return try emitAssignment(for: node)
-            
-        //case .declaration:
-            //return try emitDeclaration(for: node)
-            
-        case .procedureCall:
-            return try emitProcedureCall(for: node)
-            
-        case .conditional:
-            return try emitConditional(for: node)
-            
-        case .defer:
-            return try emitDeferStmt(for: node)
-            
-        case .operator(_):
-            return try emitOperator(for: node)
-            
-        case .return:
-            try emitReturn(for: node)
-            return VoidType().null()
-            
-        case .integer(let valueString):
-            //NOTE(Brett): should this throw?
-            let value = Int(valueString.string) ?? 0
-            return IntType.int64.constant(value)
-            
-        case .boolean(let boolean):
-            return IntType.int1.constant(boolean ? 1 : 0)
-            
-        case .string(let string):
-            return emitGlobalString(value: string)
-            
-        /*case .identifier(let identifier):
-            guard let symbol = SymbolTable.current.lookup(identifier) else {
-                fallthrough
-            }
-            
-            return builder.buildLoad(symbol.pointer!)
-          */
-            
-        default:
-            unimplemented("unsupported kind: \(node.kind)")
         }
-    }
-    #endif
-    
-    func emitConditional(for node: AstNode) -> IRValue {
-        unimplemented()
-        
-        /*guard let function = currentProcedure?.pointer else {
-            preconditionFailure("Not currently in a function")
-        }
-        
-        guard case .conditional = node.kind, node.children.count >= 2 else {
-            preconditionFailure("Expected conditional got: \(node.kind)")
-        }
-        
-        let currentBlock = builder.insertBlock!
-        let thenBody = function.appendBasicBlock(named: "then")
-        let elseBody = function.appendBasicBlock(named: "else")
-        let mergeBody = function.appendBasicBlock(named: "merge")
-        
-        builder.positionAtEnd(of: currentBlock)
-        let conditional = try emitExpression(for: node.children[0])
-        builder.buildCondBr(condition: conditional, then: thenBody, else: elseBody)
-        
-        builder.positionAtEnd(of: thenBody)
-        guard case .scope(let thenScope) = node.children[1].kind else {
-            preconditionFailure("Expected scope for `then` body.")
-        }
-        context?.scope = thenScope
-        defer {
-           // SymbolTable.pop()
-        }
-        
-        _ = try emitExpression(for: node.children[1])
-        
-        if !thenBody.hasTerminatingInstruction {
-            builder.buildBr(mergeBody)
-        }
-        
-        if !builder.insertBlock!.hasTerminatingInstruction {
-            builder.buildBr(mergeBody)
-        }
-        
-        builder.positionAtEnd(of: elseBody)
-        // has else body
-        if node.children.count >= 3 {
-            if case .scope(let elseScope) = node.children[2].kind {
-                SymbolTable.current = thenScope
-                defer {
-                    //SymbolTable.pop()
-                }
-            }
-            
-            _ = try emitExpression(for: node.children[2])
-            
-            if !elseBody.hasTerminatingInstruction {
-                builder.buildBr(mergeBody)
-            }
-            
-            if !builder.insertBlock!.hasTerminatingInstruction {
-                builder.buildBr(mergeBody)
-            }
-            
-        } else {
-            builder.buildBr(mergeBody)
-        }
-        
-        builder.positionAtEnd(of: mergeBody)
-        
-        // NOTE(Brett): do we really want to return this?
-        return conditional
-         */
+
+        //
+        // Set builder position to the end of the `if.post` block
+        //
+
+        builder.positionAtEnd(of: postBlock)
+
+        return postBlock
     }
 
     @discardableResult
@@ -450,9 +390,7 @@ extension IRGenerator {
 
             builder.positionAtEnd(of: loopCond!)
 
-            var condVal = emitStmt(for: cond)
-
-            condVal = builder.buildTrunc(condVal, type: IntType.int1)
+            let condVal = emitConditional(for: cond)
 
             builder.buildCondBr(condition: condVal, then: loopBody, else: loopDone)
         } else {
@@ -461,6 +399,8 @@ extension IRGenerator {
             }
             loopBody = curFunction.appendBasicBlock(named: "for.body")
             loopDone = curFunction.appendBasicBlock(named: "for.done")
+
+            builder.buildBr(loopBody)
         }
 
         context.escapePoints = Context.EscapePoints(break: loopDone, continue: loopPost ?? loopCond ?? loopBody)
@@ -497,6 +437,18 @@ extension IRGenerator {
         builder.positionAtEnd(of: loopDone)
 
         return loopDone
+    }
+
+    /// If a value is meant to be used as a condition use this.
+    /// It will truncate to an `i1` for you
+    func emitConditional(for node: AstNode) -> IRValue {
+        let val = emitStmt(for: node)
+
+        guard (val.type as! IntType).width == 1 else {
+            return builder.buildTrunc(val, type: IntType.int1)
+        }
+
+        return val
     }
 }
 
