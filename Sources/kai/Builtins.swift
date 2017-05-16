1ed9b3f3e65739f10ef0349a9b184c5c837cce54
Ethan Jackwitz <ethanjackwitz@gmail.com>
2017-05-17 09:12:38 +1000

    Beggining of a lot of changes

--- Sources/kai/Builtins.swift
+++ Sources/kai/Builtins.swift
@@ -1,11 +1,48 @@
 
 import LLVM
 
+extension Scope {
+
+    static let universal: Scope = {
+
+        var s = Scope(parent: nil)
+
+        // TODO(vdka): Create a stdtypes.kai file to refer to for location
+
+        for type in Type.builtin {
+
+            guard let location = type.location else {
+                panic()
+            }
+
+            let e = Entity(name: type.description, location: location, kind: .compiletime, owningScope: s)
+            e.type = type.type
+            s.insert(e)
+        }
+
+        Entity.declareBuiltinConstant(name: "true", value: .bool(true), scope: s)
+        Entity.declareBuiltinConstant(name: "false", value: .bool(false), scope: s)
+
+        var e: Entity
+        e = Entity(name: "nil", location: .unknown, kind: .compiletime, owningScope: s)
+        e.type = Type.unconstrNil
+        s.insert(e)
+
+        e = Entity(name: "_", location: .unknown, kind: .compiletime, owningScope: s)
+        e.type = Type.any
+        s.insert(e)
+
+        return s
+    }()
+}
+
 extension Type {
 
     static let builtin: [Type] = {
 
         // NOTE(vdka): Order is important later.
+
+        let platformSize = UInt(MemoryLayout<Int>.size)
                   /* Name,   size, line, flags */
         let short: [(String, UInt, UInt, Flag)] = [
             ("void", 0, 0, .none),
@@ -37,6 +74,8 @@ extension Type {
 
             ("any", 0, 0, .none),
 
+            ("rawptr", platformSize, 0, [.pointer]),
+
             ("<invalid>", 0, 0, .none),
             ("<placeholder>", 0, 0, .none),
         ]
@@ -49,9 +88,6 @@ extension Type {
             return Type(kind: .builtin(name), flags: flags, width: width, location: location)
         }
     }()
-
-    // TODO(vdka): Once we support structs define what these look like.
-    // static let typeInfo = Type(kind: .struct("TypeInfo"), flags: .none, width: 0, location: nil)
 
     static let void = builtin[0]
 
@@ -81,9 +117,53 @@ extension Type {
     static let unconstrNil      = builtin[19]
 
     static let any = builtin[20]
+    
+    static let rawptr = builtin[21]
 
-    static let invalid = builtin[21]
-    static let placeholder = builtin[22]
+    static let invalid = builtin[22]
+    static let placeholder = builtin[23]
+
+
+    var type: Type {
+        let type = Type.copy(Type.typeInfo)
+        type.kind = .type(self)
+        return type
+    }
+
+    static let typeInfo: Type = {
+        let s = Scope(parent: nil)
+
+        let name = Entity(name: "name", kind: .compiletime, type: Type.string, owningScope: s)
+        let width = Entity(name: "width", kind: .compiletime, type: Type.u64, owningScope: s)
+
+        s.insert(name)
+        s.insert(width)
+
+        var totalWidth: UInt = 0
+        for (index, entity) in s.elements.orderedValues.enumerated() {
+            entity.offsetInParent = UInt(index)
+            totalWidth += entity.type!.width
+        }
+
+        let type = Type(kind: .struct(s), flags: .none, width: totalWidth, location: .unknown)
+
+        //
+        // By making TypeInfo exist in a dummy scope it will never be possible to refer directly to it.
+        // Note that, at this point we cannot make the owning scope `.universal` because that scope 
+        //   depends upon this type.
+        // We will add `entity` to the `universal` scope and set owningScope later (in the init for `universal` scope)
+        let dummyScope = Scope(parent: nil)
+        let entity = Entity(name: "TypeInfo", kind: .compiletime, type: type, owningScope: dummyScope)
+
+        return Type.named(entity)
+    }()
+
+    static func copy(_ type: Type) -> Type {
+        let copy = Type(kind: type.kind, flags: type.flags, width: type.width, location: type.location)
+        return copy
+    }
+
+//    static let typeInfo = Type(kind: .struct("TypeInfo"), flags: .none, width: 0, location: nil)
 }
 
 
@@ -102,19 +182,18 @@ var builtinProcedures: [Entity] = {
     // These are later mapped into `[Entity]`
     let short: [Short] = [
 
-
         (
             "malloc", mangled: "malloc",
             EntityExtra(singleIrGen: IRGenerator.genForeign, callIrGen: nil),
             params: [("size", Type.i32)],
-            returns: [Type.pointer(to: Type.u8)],
+            returns: [Type.rawptr],
             isVariadic: false
         ),
         ( // NOTE(vdka): This will need to be different dependent on the type of the value.
             // if it is a dynamic Array then we need to free at a predefined offset from the pointer we are given.
             "free", mangled: "free",
             EntityExtra(singleIrGen: IRGenerator.genForeign, callIrGen: nil),
-            params: [("ptr", Type.pointer(to: Type.u8))],
+            params: [("ptr", Type.rawptr)],
             returns: [Type.void],
             isVariadic: false
         ),
@@ -142,8 +221,8 @@ var builtinProcedures: [Entity] = {
         (
             "memcpy", mangled: "memcpy",
             EntityExtra(singleIrGen: IRGenerator.genForeign, callIrGen: nil),
-            params: [("dest", Type.pointer(to: Type.void)), ("src", Type.pointer(to: Type.void)), ("len", Type.i64)],
-            returns: [Type.pointer(to: Type.void)],
+            params: [("dest", Type.rawptr), ("src", Type.rawptr), ("len", Type.i64)],
+            returns: [Type.rawptr],
             isVariadic: false
         )
     ]
@@ -157,6 +236,8 @@ var builtinProcedures: [Entity] = {
         
         let paramEntities = params.map({ Entity(name: $0.0, kind: .magic(extra), type: $0.1, owningScope: procScope) })
         entity.type = Type(kind: .proc(params: paramEntities, returns: returns, isVariadic: isVariadic), width: 0)
+
+        print(entity.type!.description)
 
         return entity
     }
