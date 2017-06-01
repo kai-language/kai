25463897bc4cf8b65081c77ea5d1d5ff920eaad7
Brett R. Toomey <brettcwx@gmail.com>
2017-06-01 21:09:27 +0200

    E and hex notation fix

--- Sources/kai/Lexer.swift
+++ Sources/kai/Lexer.swift
@@ -161,10 +161,10 @@ struct Lexer {
                 
                 return (.float(double), location)
                 
-//            case _ where number.contains("e"):
-//                return parseEnotation(Array(number.split(separator: "e")), original: number)
-//            case _ where number.contains("E"):
-//                return parseEnotation(number.split(separator: "E"), original: number)
+            case _ where number.contains("e") && !number.hasPrefix("0x"):
+                return parseEnotation(Array(number.split(separator: "e")), original: number)
+            case _ where number.contains("E") && !number.hasPrefix("0x"):
+                return parseEnotation(number.split(separator: "E"), original: number)
                 
             default:
                 guard let int = extractInteger(number) else {
