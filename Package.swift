9cc7d9260ac7826a755c054af652e42a6e00f453
Brett R. Toomey <brettcwx@gmail.com>
2017-08-02 21:30:02 +0200

    Added LLVM dependency

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ import PackageDescription
 let package = Package(
     name: "kai",
     dependencies: [
+        .package(url: "https://github.com/vdka/LLVMSwift.git", .branch("master")),
     ],
     targets: [
         .target(name: "kai", dependencies: ["Core"]),
-        .target(name: "Core", dependencies: []),
+        .target(name: "Core", dependencies: ["LLVM"]),
     ],
     swiftLanguageVersions: [4]
 )
---
+++ Sources/Core/Checker.swift
@@ -0,0 +1,5 @@
+import LLVM
+
+final class Checker {
+    
+}
