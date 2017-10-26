76f9366a30d597ca8d91ca07e850e566d100c5df
Brett R. Toomey <brettcwx@gmail.com>
2017-10-26 15:34:42 +0200

    Added distribute to build script
    Added cleanup to distribute
    
    Updated build cleanup
    
    Removed master checkout
    
    Added release package to gitignore

--- build.sh
+++ build.sh
@@ -15,6 +15,22 @@ release)
     swift build -Xswiftc -DDEBUG -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.12" -c release
     cp .build/release/kai /usr/local/bin/
 ;;
+distribute)
+    TAG=$(git describe --abbrev=0 --tags);
+    git checkout $TAG;
+    cat ./Sources/Core/Options.swift | \
+        awk -v tag="$TAG" '/public static let version = "0.0.0"/ { printf "    public static let version = \"%s\"\n", tag; next } 1' > .tmp && \
+    mv .tmp ./Sources/Core/Options.swift;
+
+    swift build -c release -Xswiftc -static-stdlib -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.12";
+
+    PACKAGE_NAME="kai-$TAG"
+    mkdir -p ./$PACKAGE_NAME
+    cp .build/release/kai ./$PACKAGE_NAME/kai
+    tar -cvzf macOS-sierra.tar.gz ./$PACKAGE_NAME
+    rm -rf $PACKAGE_NAME
+    git reset --hard HEAD
+;;
 *)
     MACOSX_DEPLOYMENT_TARGET=10.12
     swift build -Xswiftc -DDEBUG -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.12"
