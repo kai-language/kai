251b29d00e48a4eb62ab6f97d838699b9ce1902b
vdka <ethanjackwitz@gmail.com>
2018-04-01 15:37:44 +1000

    Update build script ahead of adding a test option

--- build.sh
+++ build.sh
@@ -2,8 +2,20 @@
 
 set -e
 
+debug_build() {
+    case "$platform" in
+    macOS) swift build -Xswiftc -DDEBUG -Xswiftc -DDEVELOPER -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx$MACOSX_DEPLOYMENT_TARGET" ;;
+    linux) swift build -Xswiftc -DDEBUG -Xswiftc -DDEVELOPER -Xswiftc "-target" -Xswiftc "x86_64-pc-linux-gnu" ;;
+    esac
+}
+
+unsupportedCommand() {
+    echo "Unsupported command $1"
+    exit 1
+}
+
 case "$OSTYPE" in
-  darwin*)  platform="macOS" ;; 
+  darwin*)  platform="macOS" ;;
   linux*)   platform="linux" ;;
   *)
       echo "unknown OS build script will need updating for support"
@@ -12,11 +24,6 @@ case "$OSTYPE" in
 esac
 
 MACOSX_DEPLOYMENT_TARGET=10.13
-
-unsupportedCommand() {
-    echo "Unsupported command $1"
-    exit 1
-}
 
 case "$1" in
 xcode)
@@ -27,52 +34,16 @@ xcode)
 ;;
 release)
     case "$platform" in
-        macOS) swift build -Xswiftc -DDEBUG -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx$MACOSX_DEPLOYMENT_TARGET" -c release ;;
-        linux) swift build -Xswiftc -DDEBUG -Xswiftc "-target" -Xswiftc "x86_64-pc-linux-gnu" -c release ;;
+        macOS) swift build -Xswiftc -DDEBUG -static-stdlib -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx$MACOSX_DEPLOYMENT_TARGET" -c release ;;
+        linux) swift build -Xswiftc -DDEBUG -static-stdlib -Xswiftc "-target" -Xswiftc "x86_64-pc-linux-gnu" -c release ;;
         *)
             unsupportedCommand "release"
     esac
     cp .build/release/kai /usr/local/bin/
 ;;
-distribute)
-    if [ "$platform" != "macOS" ]; then
-        unsupportedCommand "distribute"
-    fi
-    TAG=$(git describe --abbrev=0 --tags);
-    git checkout $TAG;
-    cat ./Sources/Core/Options.swift | \
-        awk -v tag="$TAG" '/public static let version = "0.0.0"/ { printf "    public static let version = \"%s\"\n", tag; next } 1' > .tmp && \
-    mv .tmp ./Sources/Core/Options.swift;
-
-    echo "building binary"
-
-    swift build -c release -Xswiftc -static-stdlib -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx$MACOSX_DEPLOYMENT_TARGET"
-    install_name_tool -change /usr/local/opt/llvm/lib/libc++.1.dylib /usr/lib/libc++.1.dylib .build/release/kai
-    PACKAGE_NAME="kai-$TAG"
-    mkdir -p ./$PACKAGE_NAME
-    cp .build/release/kai ./$PACKAGE_NAME/kai
-    tar -cvzf macOS-sierra.tar.gz ./$PACKAGE_NAME
-
-    echo "updating brew formula"
-
-    HASH=$(shasum -a 256 macOS-sierra.tar.gz | cut -d " " -f 1)
-    curl -sO https://raw.githubusercontent.com/kai-language/homebrew-tap/kai/kai.rb
-    cat kai.rb | awk -v tag="$TAG" -v hash="$HASH" '/version "*"/ { printf "  version \"%s\"\n", tag; next }/sha256/ { printf "  sha256 \"%s\"\n", hash; next } 1' > .tmp && \
-        mv .tmp kai.rb
-
-    echo "restoring working directory"
-
-    rm -rf $PACKAGE_NAME
-    git reset --hard HEAD
-;;
 *)
-
-    case "$platform" in
-        macOS) swift build -Xswiftc -DDEBUG -Xswiftc -DDEVELOPER -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx$MACOSX_DEPLOYMENT_TARGET" ;;
-        linux) swift build -Xswiftc -DDEBUG -Xswiftc -DDEVELOPER -Xswiftc "-target" -Xswiftc "x86_64-pc-linux-gnu" ;;
-    esac
+    debug_build
     cp .build/debug/kai /usr/local/bin/
 esac
 
 echo "done"
-
