18101fdb505ecb8275dc20d7c925e665d8a4339e
Brett R. Toomey <brto@nodes.dk>
2018-04-30 18:40:04 +0200

    Added entity constant for platform details

--- Sources/Core/Builtins/PlatformPackage.swift
+++ Sources/Core/Builtins/PlatformPackage.swift
@@ -5,7 +5,11 @@ extension builtin {
 
     enum platform {
 
-        static var pointerWidth: BuiltinEntity = BuiltinEntity(name: "PointerWidth", type: ty.i64) { g in
+        static var pointerWidth: BuiltinEntity = BuiltinEntity(
+            name: "PointerWidth",
+            constant: UInt64(targetMachine.dataLayout.pointerSize()),
+            type: ty.i64
+        ) { g in
             var val = g.word.constant(targetMachine.dataLayout.pointerSize() * 8)
 
             var global = g.addOrReuseGlobal(".platform.PointerWidth", initializer: val)
@@ -13,7 +17,11 @@ extension builtin {
             return global
         }
 
-        static var isBigEndian: BuiltinEntity = BuiltinEntity(name: "IsBigEndian", type: ty.bool) { g in
+        static var isBigEndian: BuiltinEntity = BuiltinEntity(
+            name: "IsBigEndian",
+            constant: (targetMachine.dataLayout.byteOrder == .bigEndian ? 1 : 0) as UInt64,
+            type: ty.bool
+        ) { g in
             let val = g.i1.constant(targetMachine.dataLayout.byteOrder == .bigEndian ? 1 : 0)
 
             var global = g.addOrReuseGlobal(".platform.IsBigEndian", initializer: val)
@@ -21,13 +29,16 @@ extension builtin {
             return global
         }
 
-        static var osTriple: BuiltinEntity = BuiltinEntity(name: "OSTriple", type: ty.string) { g in
+        static var osTriple: BuiltinEntity = BuiltinEntity(
+            name: "OSTriple",
+            constant: targetMachine.triple,
+            type: ty.string
+        ) { g in
             let val = g.emit(constantString: targetMachine.triple)
 
             var global = g.addOrReuseGlobal(".platform.OSTriple", initializer: val)
             global.linkage = .private
             return global
         }
-
     }
 }
